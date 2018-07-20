2e7ddf98cc7a33e756c345141a48baa10c614e5d
zsh <zshui9@163.com>
2018-07-20 17:44:34 +0800

    weqwqw

---
+++ src/main/java/com/server/business/controller/ConfigController.java
@@ -0,0 +1,22 @@
+package com.server.business.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 创建会员接口
+ *
+ * @author Terry Zhang
+ * @date 2018-07-20 九月 11:10
+ * @modify
+ **/
+
+@RestController
+@RequestMapping("/config")
+public class ConfigController {
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+}
